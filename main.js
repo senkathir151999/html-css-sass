bfe55cd8b764aa80721735a4b2fac0829fb80d71
senkathir15199 <senkathirmathi15@gmail.com>
2020-08-17 18:34:15 +0530

    java input console

---
+++ main.js
@@ -0,0 +1,20 @@
+function toggleMobileMenu() {
+  document.querySelector("#menu").classList.toggle("active");
+  document.querySelector(".toggle").classList.toggle("active");
+}
+function validate() {
+  console.log(document.getElementById("input-1").value);
+}
+
+
+function validate2() {
+  console.log(document.getElementById("input-2").value);
+}
+function validate3() {
+  console.log(document.getElementById("input-3").value);
+  console.log(document.getElementById("input-4").value);
+  console.log(document.getElementById("input-5").value);
+}
+function validate4() {
+  console.log(document.getElementById("input-6").value);
+}
