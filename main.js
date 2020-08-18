80ecf0ff98ea42c4309ac04be04486a21bfdd254
senkathir15199 <senkathirmathi15@gmail.com>
2020-08-18 13:59:49 +0530

    validate email

--- main.js
+++ main.js
@@ -3,18 +3,74 @@ function toggleMobileMenu() {
   document.querySelector(".toggle").classList.toggle("active");
 }
 function validate() {
-  console.log(document.getElementById("input-1").value);
-}
+  var x = document.getElementById("input-1").value;
 
+  var atposition = x.indexOf("@");
+  var dotposition = x.lastIndexOf(".");
+  if (x == "") {
+    alert(" please Enter your Email");
+  } else if (
+    atposition < 1 ||
+    dotposition < atposition + 2 ||
+    dotposition + 2 >= x.length
+  )
+   {
+    alert("please enter valid email");
+  } else {
+    console.log(document.getElementById("input-1").value);
+  }
+}
 
 function validate2() {
-  console.log(document.getElementById("input-2").value);
+  var y = document.getElementById("input-2").value;
+
+  var atposition = y.indexOf("@");
+  var dotposition = y.lastIndexOf(".");
+  if (y == "") {
+    alert(" please Enter your Email");
+  } else if (
+    atposition < 1 ||
+    dotposition < atposition + 2 ||
+    dotposition + 2 >= y.length
+  ) {
+    alert("Email must be  valid");
+  } else {
+    console.log(document.getElementById("input-2").value);
+  }
 }
 function validate3() {
-  console.log(document.getElementById("input-3").value);
-  console.log(document.getElementById("input-4").value);
-  console.log(document.getElementById("input-5").value);
+  var x = document.getElementById("input-3").value;
+  if (x == "") {
+    alert("Name must be  given");
+  } else {
+    console.log(document.getElementById("input-3").value);
+  }
+  var y = document.getElementById("input-4").value;
+  if (y == "") {
+    alert("Email must be  given");
+  } else {
+    console.log(document.getElementById("input-4").value);
+  }
+  var z = document.getElementById("input-5").value;
+  if (z == "") {
+    alert("Description must be  given");
+  } else {
+    console.log(document.getElementById("input-5").value);
+  }
 }
 function validate4() {
-  console.log(document.getElementById("input-6").value);
+  var x = document.getElementById("input-6").value;
+  var atposition = x.indexOf("@");
+  var dotposition = x.lastIndexOf(".");
+  if (x == "") {
+    alert("please Enter your Email");
+  } else if (
+    atposition < 1 ||
+    dotposition < atposition + 2 ||
+    dotposition + 2 >= x.length
+  ) {
+    alert("please check your Email");
+  } else {
+    console.log(document.getElementById("input-6").value);
+  }
 }
